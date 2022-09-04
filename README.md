3f78211f35c6bdb219889a668f858ec971f2f2ed
wuhongbo4 <wuhongbo4@juneyaoair.com>
2022-09-04 15:51:23 +0800

    init: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Template
+
+rainbowatcher's node starter template
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('test suit', () => {
+  it('must pass', () => {
+    expect(true).toMatchInlineSnapshot('true')
+  })
+})
